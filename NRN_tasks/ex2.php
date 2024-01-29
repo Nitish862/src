aa3a9ad4b8df91430cd25323d3b244df5b175673
Nitish <nitish.neupane862@gmail.com>
2024-01-29 16:56:41 +0200

    done

---
+++ NRN_tasks/ex2.php
@@ -0,0 +1,37 @@
+<?php include 'header.php'; ?>
+<div class="container">
+        <h1 class="header">Vacancy Announcement</h1>
+
+        <table class="vacancy-table">
+            <thead>
+                <tr>
+                    <th>Post</th>
+                    <th>Time</th>
+                    <th>Requirement</th>
+                    <th>Deadline</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Front Desk Officer</td>
+                    <td>Full-time</td>
+                    <td>3 Female/ 2 male</td>
+                    <td>25-1-2024</td>
+                </tr>
+                <tr>
+                    <td>Waiter</td>
+                    <td>Part-time</td>
+                    <td>5 male</td>
+                    <td>30-2-2024</td>
+                </tr>
+                <tr>
+                    <td>Cleaner</td>
+                    <td>Full-time</td>
+                    <td>1 male</td>
+                    <td>28-1-2024</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+<?php include 'footer.php'; ?>
