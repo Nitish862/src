2c1488c3c633edd00d565cbe158be0bef340d76f
Nitish <nitish.neupane862@gmail.com>
2024-02-08 02:59:57 +0200

    done

---
+++ NRN_tasks/ex7.php
@@ -0,0 +1,15 @@
+<?php
+include 'header.php';
+?>
+
+<h1>View Or Read</h1>
+
+<nav>
+<a href="./crud/index.php">view form</a>
+
+    <a href="./crud/read.php">view  data</a>
+</nav>
+
+<?php
+include 'footer.php';
+?>
